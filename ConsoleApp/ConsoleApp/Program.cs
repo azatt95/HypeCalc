8ba3bd52fff43ee5490d7411d193a54feced2805
varankin anatoliy <varankin@elewise.com>
2017-10-30 11:33:06 +0400

    Первая версия / не корректно считает

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -8,9 +8,27 @@ namespace ConsoleApp
 {
     class Program
     {
+        // operation x y
         static void Main(string[] args)
         {
             Console.WriteLine("Hype Calc");
+
+            if (args.Count() == 3)
+            {
+                var operation = args[0];
+                var x = args[1];
+                var y = args[2];
+                var result = "";
+
+
+                if (operation == "sum")
+                {
+                    result = x + y;
+                }
+
+                Console.WriteLine(result);
+            }
+
             Console.ReadKey();
         }
     }
